858ab0f118631e66c026a604bec7515547c8e203
florent kaisser <florent.kaisser@free.fr>
2010-09-21 17:21:04 +0200

    draw a car on the carte center

--- gps/src/GpsMonitor.java
+++ gps/src/GpsMonitor.java
@@ -1,20 +1,27 @@
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 import com.roots.swtmap.MapWidget;
+import com.roots.swtmap.MapWidget.OverImage;
 
 
 public class GpsMonitor {
 	static MapWidget map;
 	static Display display;
 	static Gps gps;
+	static Image car;
 	
 	public static Point WGS84toPoint(WGS84 position,int z){
 		return new Point(MapWidget.lon2position(position.longitude(), z), MapWidget.lat2position(position.latitude(), z));
+	}
+	
+	public static Image loadCarImage(){
+		return new Image(display, "res/twingo.png");
 	}
 
     public static void main (String [] args) throws Exception {
@@ -29,9 +36,10 @@ public class GpsMonitor {
       
       
       map = new MapWidget(shell, SWT.NONE);
-      
-      gps = new Gps();
+      map.addOverImage( map.new OverImage(0, 0, loadCarImage()) ); 
 
+    		  
+      gps = new Gps();
       gps.addPositionListener(new GpsListener() {
 
     	  public void positionChanged(WGS84 position) {
